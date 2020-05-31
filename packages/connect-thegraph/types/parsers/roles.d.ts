61374e62fc2fa158adab19f0f01557f1b55e3dcf
Pierre Bertet <hello@pierre.world>
2020-05-31 02:02:31 +0100

    Remove unused types/ directory

--- packages/connect-thegraph/types/parsers/roles.d.ts
+++
@@ -1,5 +0,0 @@
-import { RoleData } from '@aragon/connect-core';
-import { QueryResult } from '../types';
-export declare function parseRole(result: QueryResult): RoleData;
-export declare function parseRoles(result: QueryResult): RoleData[];
-//# sourceMappingURL=roles.d.ts.map
--- packages/connect-thegraph/types/types.d.ts
+++
@@ -1,5 +0,0 @@
-import { OperationResult } from "@urql/core";
-export declare type QueryResult = OperationResult<any>;
-export declare type DataGql = any;
-export declare type ParseFunction = (data: DataGql) => {};
-//# sourceMappingURL=types.d.ts.map
