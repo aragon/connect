3bddc3af27e33af071ca4d4f298230e62e426fed
Pierre Bertet <hello@pierre.world>
2020-05-31 21:07:05 +0100

    Dependencies cleanup (#5)

--- packages/connect-thegraph-token-manager/src/index.ts
+++ packages/connect-thegraph-token-manager/src/index.ts
@@ -1,6 +1,3 @@
-// required by urql, see https://github.com/FormidableLabs/urql/issues/283#issuecomment-500144070
-import 'isomorphic-unfetch'
-
 export { default as TokenManagerConnectorTheGraph } from './connector'
 
 export { default as TokenManager } from './entities/TokenManager'
