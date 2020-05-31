b80c855054e5ac15000942e1f19a8c3e86c89c32
Pierre Bertet <hello@pierre.world>
2020-05-31 21:13:08 +0100

    Org Viewer Web: default to mainnet (#7)

--- examples/organization-viewer-web/src/environment.ts
+++ examples/organization-viewer-web/src/environment.ts
@@ -3,7 +3,7 @@ type EnvType = {
   daoSubgraphUrl: string
 }
 
-const ENV_NAME = 'MAINNET_STAGING'
+const ENV_NAME = 'MAINNET'
 
 const ENVIRONMENTS = new Map([
   [
@@ -12,7 +12,9 @@ const ENVIRONMENTS = new Map([
       daoSubgraphUrl:
         'https://api.thegraph.com/subgraphs/name/aragon/aragon-mainnet',
       addresses: new Map([
-        ['a1', '0x635193983512c621E6a3E15ee1dbF36f0C0Db8E0'],
+        ['a1.aragonid.eth', '0x635193983512c621E6a3E15ee1dbF36f0C0Db8E0'],
+        ['governance.aragonproject.eth', '0x2dE83b50Af29678774D5AbC4a7Cb2a588762f28C'],
+        ['brightid.aragonid.eth', '0x5aAd137d8f7D2Dc6e1B2548c059B1483360bCC6a'],
       ]),
     },
   ],
@@ -22,7 +24,7 @@ const ENVIRONMENTS = new Map([
       daoSubgraphUrl:
         'https://api.thegraph.com/subgraphs/name/aragon/aragon-mainnet-staging',
       addresses: new Map([
-        ['piedao', '0x0c188b183ff758500d1d18b432313d10e9f6b8a4'],
+        ['piedao.aragonid.eth', '0x0c188b183ff758500d1d18b432313d10e9f6b8a4'],
       ]),
     },
   ],
@@ -32,8 +34,8 @@ const ENVIRONMENTS = new Map([
       daoSubgraphUrl:
         'https://api.thegraph.com/subgraphs/name/aragon/aragon-rinkeby',
       addresses: new Map([
-        ['org1', '0x0146414e5a819240963450332f647dfb7c722af4'],
-        ['org2', '0x00018d22ece8b2ea4e9317b93f7dff67385693d8'],
+        ['org1.aragonid.eth', '0x0146414e5a819240963450332f647dfb7c722af4'],
+        ['org2.aragonid.eth', '0x00018d22ece8b2ea4e9317b93f7dff67385693d8'],
         ['td.aragonid.eth', '0xa9Aad8e278eECf369c42F78D5A3f2d866DE902C8'],
         ['hive.aragonid.eth', '0xe520428C232F6Da6f694b121181f907931fD2211'],
         ['mesh.aragonid.eth', '0xa48300a4E89b59A79452Db7d3CD408Df57f4aa78'],
