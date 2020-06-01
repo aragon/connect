da476f344cd41cb6e12893915a9af2af6894db02
Pierre Bertet <hello@pierre.world>
2020-06-01 02:14:33 +0100

    Address review comments (#10)
    - Imports order.
    - Remove unused dependencies.

--- packages/connect-core/src/index.ts
+++ packages/connect-core/src/index.ts
@@ -3,10 +3,8 @@ export {
   default as ConnectorJson,
   ConnectorJsonConfig,
 } from './connections/ConnectorJson'
-
-// TODO: Use index.ts in src/wrappers instead?
-export { default as Organization } from './entities/Organization'
 export { default as App, AppData } from './entities/App'
+export { default as Organization } from './entities/Organization'
+export { default as Permission, PermissionData } from './entities/Permission'
 export { default as Repo, RepoData } from './entities/Repo'
 export { default as Role, RoleData } from './entities/Role'
-export { default as Permission, PermissionData } from './entities/Permission'
