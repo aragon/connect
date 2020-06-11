5e166a4d6089a92f6eedb73f89edf086a0647ee5
0xGabi <gabrielpk.18@gmail.com>
2020-06-11 12:54:13 -0300

    Include radspec description logic

---
+++ packages/connect-core/src/utils/radspec/index.ts
@@ -0,0 +1,78 @@
+import { ethers } from 'ethers'
+import * as radspec from 'radspec-gabi'
+
+import { addressesEqual } from '../address'
+import { findAppMethodFromIntent } from '../app'
+import Application from '../../entities/Application'
+import { TransactionRequestData } from '../../transactions/TransactionRequest'
+import { Abi, AppIntent } from '../../types'
+
+interface FoundMethod {
+  method?: AppIntent
+  abi?: Abi
+}
+
+/**
+ * Attempt to describe intent via radspec.
+ */
+export async function tryEvaluatingRadspec(
+  intent: TransactionRequestData,
+  apps: Application[],
+  provider?: ethers.providers.Provider // Decorated intent with description, if one could be made
+): Promise<TransactionRequestData> {
+  const app = apps.find((app) => addressesEqual(app.address, intent.to))
+
+  // If the intent matches an installed app, use only that app to search for a
+  // method match, otherwise fallback to searching all installed apps
+  const appsToSearch = app ? [app] : apps
+  const foundMethod = appsToSearch.reduce<FoundMethod | undefined>(
+    (found, app) => {
+      if (found) {
+        return found
+      }
+
+      const method = findAppMethodFromIntent(app, intent)
+      if (method) {
+        return {
+          method,
+          // This is not very nice, but some apps don't have ABIs attached to their function
+          // declarations and so we have to fall back to using their full app ABI
+          // TODO: define a more concrete schema around the artifact.json's `function.abi`
+          abi: method.abi ? [method.abi] : app.abi,
+        }
+      }
+    },
+    undefined
+  )
+
+  const { abi, method } = foundMethod || {}
+
+  let evaluatedNotice
+  if (method && method.notice) {
+    try {
+      evaluatedNotice = await radspec.evaluate(
+        method.notice,
+        {
+          abi,
+          transaction: intent,
+        },
+        { provider: provider }
+      )
+    } catch (err) {
+      console.error(
+        `Could not evaluate a description for given transaction data: ${intent.data}`,
+        err
+      )
+    }
+  }
+
+  return {
+    ...intent,
+    description: evaluatedNotice,
+  }
+}
+
+export {
+  postprocessRadspecDescription,
+  PostProcesDescription,
+} from './postprocess'
---
+++ packages/connect-core/src/utils/radspec/postprocess.ts
@@ -0,0 +1,126 @@
+import { addressesEqual, ANY_ENTITY } from '../address'
+import { getKernelNamespace } from '../kernel'
+import Application from '../../entities/Application'
+import Role from '../../entities/Role'
+import { Annotation } from '../../transactions/TransactionRequest'
+
+export interface PostProcesDescription {
+  description: string
+  annotatedDescription?: Annotation[]
+}
+
+interface CompiledTokens {
+  description: string[]
+  annotatedDescription: Annotation[]
+}
+
+type ProcessToken = [string, string, Annotation]
+
+/**
+ * Look for known addresses and roles in a radspec description and substitute them with a human string
+ *
+ * @param  {string} description
+ * @return {Promise<Object>} Description and annotated description
+ */
+export async function postprocessRadspecDescription(
+  description: string,
+  apps: Application[]
+): Promise<PostProcesDescription> {
+  const addressRegexStr = '0x[a-fA-F0-9]{40}'
+  const addressRegex = new RegExp(`^${addressRegexStr}$`)
+  const bytes32RegexStr = '0x[a-f0-9]{64}'
+  const bytes32Regex = new RegExp(`^${bytes32RegexStr}$`)
+  const combinedRegex = new RegExp(
+    `\\b(${addressRegexStr}|${bytes32RegexStr})\\b`
+  )
+
+  const tokens = description
+    .split(combinedRegex)
+    .map((token) => token.trim())
+    .filter((token) => token)
+
+  if (tokens.length <= 1) {
+    return { description }
+  }
+
+  const roles: Role[] = []
+  for (const app of apps) {
+    const appRoles = await app.roles()
+    roles.concat(appRoles)
+  }
+
+  const annotateAddress = (input: string): ProcessToken => {
+    if (addressesEqual(input, ANY_ENTITY)) {
+      return [
+        input,
+        "'Any account'",
+        { type: 'any-account', value: ANY_ENTITY },
+      ]
+    }
+
+    const app = apps.find(({ address }) => addressesEqual(address, input))
+    if (app) {
+      const replacement = `${app.name}${app.appId ? ` (${app.appId})` : ''}`
+      return [input, `'${replacement}'`, { type: 'app', value: app }]
+    }
+
+    return [input, input, { type: 'address', value: input }]
+  }
+
+  const annotateBytes32 = (input: string): ProcessToken => {
+    const role = roles.find(({ hash }) => hash === input)
+
+    if (role && role.name) {
+      return [input, `'${role.name}'`, { type: 'role', value: role }]
+    }
+
+    const app = apps.find(({ appId }) => appId === input)
+
+    if (app) {
+      // return the entire app as it contains APM package details
+      return [input, `'${app.appName}'`, { type: 'apmPackage', value: app }]
+    }
+
+    const namespace = getKernelNamespace(input)
+    if (namespace) {
+      return [
+        input,
+        `'${namespace.name}'`,
+        { type: 'kernelNamespace', value: namespace },
+      ]
+    }
+
+    return [input, input, { type: 'bytes32', value: input }]
+  }
+
+  const annotateText = (input: string): ProcessToken => {
+    return [input, input, { type: 'text', value: input }]
+  }
+
+  const annotatedTokens = tokens.map((token) => {
+    if (addressRegex.test(token)) {
+      return annotateAddress(token)
+    }
+    if (bytes32Regex.test(token)) {
+      return annotateBytes32(token)
+    }
+    return annotateText(token)
+  })
+
+  const compiled: CompiledTokens = annotatedTokens.reduce(
+    (acc, [_, replacement, annotation]) => {
+      acc.description.push(replacement)
+      acc.annotatedDescription.push(annotation)
+      return acc
+    },
+    {
+      annotatedDescription: [] as Annotation[],
+      description: [] as string[],
+    }
+  )
+
+  return {
+    annotatedDescription: compiled.annotatedDescription,
+    description: compiled.description.join(' '),
+  }
+}
