51fd26a7aa8920fd5e9a40ef30d00b5742955d79
Mathew Cormier <mathew.corm@gmail.com>
2020-06-12 13:11:42 -0400

    Improve subscription-voting

--- examples/organization-viewer-cli/src/subscriptions-voting.ts
+++ examples/organization-viewer-cli/src/subscriptions-voting.ts
@@ -1,6 +1,8 @@
 import { Voting, Vote, Cast } from '@aragon/connect-thegraph-voting'
+import { keepRunning } from './helpers'
 
-const VOTING_APP_ADDRESS = '0xc73e86aab9d232495399d62fc80a36ae52952b81'
+const ORG_ADDRESS = '0x7cee20f778a53403d4fc8596e88deb694bc91c98'
+const VOTING_APP_ADDRESS = '0xf7f9a33ed13b01324884bd87137609251b5f7c88'
 const ALL_VOTING_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/aragon/aragon-voting-rinkeby'
 
 async function main() {
@@ -10,16 +12,19 @@ async function main() {
   )
 
   const votesSubscription = voting.onVotes((votes: Vote[]) => {
+    console.log('\nVotes: ')
     votes.map(console.log)
+    console.log(`\nTry creating a new vote at https://rinkeby.aragon.org/#/${ORG_ADDRESS}/${VOTING_APP_ADDRESS}/\n`)
   })
 
   const votes = await voting.votes()
-  const lastVote = votes[votes.length - 1]
-  console.log(`last`, lastVote)
+  const vote1 = votes[1]
+  console.log(`Vote #1: `, vote1)
 
-  const castsSubscription = lastVote.onCasts((casts: Cast[]) => {
+  const castsSubscription = vote1.onCasts((casts: Cast[]) => {
     console.log(`\nCasts:`)
     casts.map(console.log)
+    console.log(`\nTry casting a vote on https://rinkeby.aragon.org/#/${ORG_ADDRESS}/${VOTING_APP_ADDRESS}/vote/1 (You must first mint yourself a token in the Token Manager)\n`)
   })
 
   await keepRunning()
@@ -29,17 +34,11 @@ async function main() {
   castsSubscription.unsubscribe()
 }
 
-async function keepRunning() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve()
-    }, 1000000000)
-  })
-}
 
 main()
   .then(() => process.exit(0))
   .catch((err) => {
-    console.log(`err`, err)
+    console.log(`Error: `, err)
+    console.log('\nPlease report any problem to https://github.com/aragon/connect/issues')
     process.exit(1)
   })
