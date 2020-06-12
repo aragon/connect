76cbb224be2a16d3e10959409969e9a5be5143a9
Mathew Cormier <mathew.corm@gmail.com>
2020-06-12 13:18:20 -0400

    Improve transaction-path

--- examples/organization-viewer-cli/src/transaction-path.ts
+++ examples/organization-viewer-cli/src/transaction-path.ts
@@ -5,6 +5,8 @@ const network = 'mainnet'
 
 const DAO_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/aragon/aragon-mainnet'
 const ORG_ADDRESS = '0x0c188b183ff758500d1d18b432313d10e9f6b8a4'
+const ACCOUNT = '0xf76604Ce7e7F0134a5310bCfc9C34cAEddf15873'
+
 
 async function main() {
   const readProvider = ethers.getDefaultProvider(network)
@@ -18,16 +20,14 @@ async function main() {
   const apps = await org.apps()
   const finance = apps.find((app: Application) => app.name == 'finance')!
 
-  const account = '0xf76604Ce7e7F0134a5310bCfc9C34cAEddf15873'
-
   const intent = org.appIntent(finance.address, 'newImmediatePayment', [
     ethers.constants.AddressZero,
-    account,
+    ACCOUNT,
     ethers.utils.parseEther('1'),
     'Tests Payment',
   ])
 
-  const txPath = await intent.paths(account)
+  const txPath = await intent.paths(ACCOUNT)
 
   console.log('\nTransactions on the path:')
   txPath.transactions.map((tx: any) => console.log(tx))
@@ -36,6 +36,7 @@ async function main() {
 main()
   .then(() => process.exit(0))
   .catch((err) => { 
-    console.log(`err`, err)
+    console.log(`Error: `, err)
+    console.log('\nPlease report any problem to https://github.com/aragon/connect/issues')
     process.exit(1)
   })
