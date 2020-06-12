5977092592d92fe1a4ab194a868e5cf9b4296d87
0xGabi <gabrielpk.18@gmail.com>
2020-06-12 06:40:13 -0300

    Update dependencies and include one test

--- packages/connect-core/src/utils/__test__/address.test.ts
+++ packages/connect-core/src/utils/__test__/address.test.ts
@@ -1,6 +1,6 @@
-import { addressesEqual } from '../address'
+import { addressesEqual, isAddress, ANY_ENTITY } from '../address'
 
-test('Should check if string is an Ethereum address', () => {
+test('Should check if two Ethereum addresses are equal', () => {
   expect(
     addressesEqual(
       '0xb4124cEB3451635DAcedd11767f004d8a28c6eE7',
@@ -9,9 +9,14 @@ test('Should check if string is an Ethereum address', () => {
   ).toBe(true)
 
   expect(
-    addressesEqual(
-      '0xb4124cEB3451635DAcedd11767f004d8a28c6eE7',
-      '0xg4124cEB3451635DAcedd11767f004d8a28c6eE7'
-    )
+    addressesEqual('0xb4124cEB3451635DAcedd11767f004d8a28c6eE7', ANY_ENTITY)
   ).toBe(false)
+})
+
+test('Should check if two Ethereum addresses are equal', () => {
+  expect(isAddress('dumy')).toBe(false)
+
+  expect(
+    isAddress('0xb4124cEB3451635DAcedd11767f004d8a28c6eE7'.toLowerCase())
+  ).toBe(true)
 })
