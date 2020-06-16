16821216170f5668a4190bd2cb3a8bfc23e0184e
Pierre Bertet <hello@pierre.world>
2020-06-16 18:31:17 +0100

    nodejs demo: move to chainId

--- examples/nodejs/src/subscriptions-org.ts
+++ examples/nodejs/src/subscriptions-org.ts
@@ -1,18 +1,18 @@
 import { connect, Permission, Organization } from '@aragon/connect'
 import { keepRunning } from './helpers'
 
-const DAO_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/aragon/aragon-rinkeby'
 const ORG_ADDRESS = '0xd697d1f417c621a6a54d9a7fa2805596ca393222'
 
 async function main() {
-  const org = (await connect(
-    ORG_ADDRESS,
-    ['thegraph', { daoSubgraphUrl: DAO_SUBGRAPH_URL }]
-  )) as Organization
+  const org = (await connect(ORG_ADDRESS, 'thegraph', {
+    chainId: 4,
+  })) as Organization
 
   const subscription = org.onPermissions((permissions: Permission[]) => {
     permissions.map(console.log)
-    console.log(`\nTry creating or granting new permissions at https://rinkeby.aragon.org/#/${ORG_ADDRESS}/permissions/`)
+    console.log(
+      `\nTry creating or granting new permissions at https://rinkeby.aragon.org/#/${ORG_ADDRESS}/permissions/`
+    )
   })
 
   await keepRunning()
@@ -21,12 +21,12 @@ async function main() {
   subscription.unsubscribe()
 }
 
-
-
 main()
   .then(() => process.exit(0))
-  .catch((err) => {
+  .catch(err => {
     console.log(`Error: `, err)
-    console.log('\nPlease report any problem to https://github.com/aragon/connect/issues')
+    console.log(
+      '\nPlease report any problem to https://github.com/aragon/connect/issues'
+    )
     process.exit(1)
   })
