e98c4bb0c33551df0583b471192370adb7ec5884
ajs <palebluedot@gmail.com>
2020-06-24 13:26:24 -0300

    Renamed token-manager connector to tokens

--- examples/nodejs/src/tokens.ts
+++ examples/nodejs/src/tokens.ts
@@ -1,4 +1,4 @@
-import { TokenManager } from '@aragon/connect-thegraph-token-manager'
+import { TokenManager } from '@aragon/connect-thegraph-tokens'
 
 const TOKENS_APP_ADDRESS = '0xb27004bf714ce2aa38f14647b38836f26df86cbf'
 const ALL_TOKEN_MANAGER_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/aragon/aragon-tokens-mainnet'
