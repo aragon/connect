1f4be11cf89d66b634519d445c40c21651e04988
ajs <palebluedot@gmail.com>
2020-06-24 12:53:57 -0300

    Added a large global timeout for tests

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
   ...baseConfig,
   projects: ['<rootDir>/packages/*/jest.config.js'],
   coverageDirectory: '<rootDir>/coverage/',
+  testTimeout: 120000,
   collectCoverageFrom: ['<rootDir>/packages/*/src/**/*.{ts,tsx}'],
 }
