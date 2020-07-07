030eb64f5c9db799d9a13809e52f4b8dfc18ca54
0xGabi <gabrielpk.18@gmail.com>
2020-07-07 14:40:29 -0300

    Remove SimpleVote component

--- examples/connect-react-intro/src/App.tsx
+++ examples/connect-react-intro/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Connect } from '@aragon/connect-react'
 import Info from './Info'
-import SimpleVote from './SimpleVote'
 
 function App() {
   return (
     <Connect location="a1.aragonid.eth" connector="thegraph">
       <Info />
-      {/*<SimpleVote />*/}
     </Connect>
   )
 }
