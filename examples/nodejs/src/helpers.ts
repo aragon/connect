e48b2dad5c2e21162267c78b0decce856e2c8728
Gabriel Garcia <gabrielpk.18@gmail.com>
2020-07-08 17:10:49 -0300

    Add example to create an organization (#124)

--- examples/nodejs/src/helpers.ts
+++ examples/nodejs/src/helpers.ts
@@ -1,7 +1,52 @@
+import gql from 'graphql-tag'
+import { ethers } from 'ethers'
+import { GraphQLWrapper } from '@aragon/connect-thegraph'
+
 export async function keepRunning() {
   return new Promise(resolve => {
     setTimeout(() => {
       resolve()
     }, 1000000000)
+  })
+}
+
+const QUERY_REPO_BY_NAME = gql`
+  query RepoData($name: String) {
+    repos(where: { name: $name }) {
+      id
+      name
+      lastVersion {
+        codeAddress
+        artifact
+        semanticVersion
+      }
+    }
+  }
+`
+
+export async function fetchRepo(name: string, subgraph: string) {
+  // Create the GraphQL wrapper using the specific Subgraph URL
+  const wrapper = new GraphQLWrapper(subgraph)
+
+  // Invoke the custom query and receive data
+  return wrapper.performQuery(QUERY_REPO_BY_NAME, {
+    name,
+  })
+}
+
+export async function getOrgAddress(
+  selectedFilter: string,
+  templateContract: ethers.Contract,
+  transactionHash: string
+): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const filter = templateContract.filters[selectedFilter]()
+
+    templateContract.on(filter, (contractAddress, event) => {
+      if (event.transactionHash === transactionHash) {
+        templateContract.removeAllListeners()
+        resolve(contractAddress)
+      }
+    })
   })
 }
