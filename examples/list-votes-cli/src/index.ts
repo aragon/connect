beaa976793c711545810bdafee61dd22eded1101
Pierre Bertet <hello@pierre.world>
2020-07-14 14:24:58 +0100

    Add a simple demo to list votes

---
+++ examples/list-votes-cli/src/index.ts
@@ -0,0 +1,61 @@
+import { connect } from '@aragon/connect'
+import votingConnector from '@aragon/connect-thegraph-voting'
+
+const BLUE = '\x1b[36m'
+const RESET = '\x1b[0m'
+
+const env = {
+  chainId: 4,
+  location: 'gardens.aragonid.eth',
+}
+
+// const env = {
+//   chainId: 1,
+//   location: 'governance.aragonproject.eth',
+// }
+
+async function main() {
+  const org = await connect(env.location, 'thegraph', { chainId: env.chainId })
+
+  console.log('')
+  printOrganization(org)
+
+  const votingInfo = await org.app('voting')
+  const voting = await votingInfo.connect(votingConnector())
+  const votes = await voting.votes()
+  printVotes(votes)
+}
+
+function printOrganization(organization: any) {
+  console.log(' Organization')
+  console.log('')
+  console.log('  Location:', BLUE + organization.location + RESET)
+  console.log('  Address:', BLUE + organization.address + RESET)
+  console.log('')
+}
+
+function printVotes(votes: any) {
+  console.log(` Votes (${votes.length})`)
+  console.log('')
+  for (const vote of votes) {
+    console.log('  Vote:', `${BLUE}${formatVote(vote)}${RESET}`)
+  }
+  console.log('')
+}
+
+function formatVote(vote: any): string {
+  let str = vote.metadata
+  str = str.replace(/\n/g, ' ')
+  return str.length > 60 ? str.slice(0, 60) + '…' : str
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(err => {
+    console.error('')
+    console.error(err)
+    console.log(
+      'Please report any problem to https://github.com/aragon/connect/issues'
+    )
+    process.exit(1)
+  })
