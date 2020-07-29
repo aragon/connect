e095d5c6d21a711bb18dbeee55d7fe70a91bebcb
0xGabi <gabrielpk.18@gmail.com>
2020-07-29 14:06:18 -0300

    Connectors: Use official aragon subgraphs url

--- packages/connect-voting/src/thegraph/connector.ts
+++ packages/connect-voting/src/thegraph/connector.ts
@@ -14,7 +14,7 @@ export function subgraphUrlFromChainId(chainId: number) {
     return 'https://api.thegraph.com/subgraphs/name/aragon/aragon-voting-rinkeby'
   }
   if (chainId === 100) {
-    return 'https://api.thegraph.com/subgraphs/name/0xgabi/aragon-voting-xdai'
+    return 'https://api.thegraph.com/subgraphs/name/aragon/aragon-voting-xdai'
   }
   return null
 }
