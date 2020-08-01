6a1e397184c5b06ca1ea622fdd60b54c2a356707
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-08-01 17:15:16 -0300

    Agreement: Fix tests

--- packages/connect-thegraph-agreement/src/__test__/versions.test.ts
+++ packages/connect-thegraph-agreement/src/__test__/versions.test.ts
@@ -1,7 +1,7 @@
 import { AgreementConnectorTheGraph, Version } from '../../src'
 
 const AGREEMENT_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/facuspagnuolo/aragon-agreement-rinkeby-staging'
-const AGREEMENT_APP_ADDRESS = '0x5c6620c49f9aecf74bd483054f2d0ace0d375f96'
+const AGREEMENT_APP_ADDRESS = '0x40bb5003d24a0f58da03b19287e20ce2a3db9b54'
 
 describe('Agreement versions', () => {
   let connector: AgreementConnectorTheGraph
@@ -20,17 +20,17 @@ describe('Agreement versions', () => {
     let version: Version
 
     beforeAll(async () => {
-      version = await connector.version('1', 1000, 0)
+      version = await connector.version('3', 1000, 0)
     })
 
     test('returns that version information', () => {
-      expect(version.id).toBe(`${AGREEMENT_APP_ADDRESS}-version-1`)
-      expect(version.versionId).toBe("1")
-      expect(version.title).toBe("Test Agreement")
-      expect(version.content).toEqual('0x697066733a516d6235434862515151783659586b504536486f646558566d744352677053676b6a39456b57397873366a44486a')
-      expect(version.arbitrator).toBe('0x06a3fa06f9bfa8d945c367d183d1562bce0500db')
-      expect(version.appFeesCashier).toBe('0x45c8e37ef5bb4c6681351282d7d0ceda58bb7eb0')
-      expect(version.effectiveFrom).toBe('1594074954')
+      expect(version.id).toBe(`${AGREEMENT_APP_ADDRESS}-version-3`)
+      expect(version.versionId).toBe("3")
+      expect(version.title).toBe("Agreement Test v3")
+      expect(version.content).toEqual('0x697066733a516d644c7533585854397555597871444b5858735459473737714e594e5062687a4c32375a5954396b457271635a')
+      expect(version.arbitrator).toBe('0x52180af656a1923024d1accf1d827ab85ce48878')
+      expect(version.appFeesCashier).toBe('0x0000000000000000000000000000000000000000')
+      expect(version.effectiveFrom).toBe('1596201178')
     })
   })
 
@@ -46,13 +46,13 @@ describe('Agreement versions', () => {
     })
 
     test('allows fetching a single version', () => {
-      let version = versions[0]
+      const version = versions[0]
 
-      expect(version.title).toBe("Test Agreement")
-      expect(version.content).toEqual('0x697066733a516d6235434862515151783659586b504536486f646558566d744352677053676b6a39456b57397873366a44486a')
-      expect(version.arbitrator).toBe('0x06a3fa06f9bfa8d945c367d183d1562bce0500db')
-      expect(version.appFeesCashier).toBe('0x45c8e37ef5bb4c6681351282d7d0ceda58bb7eb0')
-      expect(version.effectiveFrom).toBe('1594074954')
+      expect(version.title).toBe("Agreement Test")
+      expect(version.content).toEqual('0x697066733a516d644c7533585854397555597871444b5858735459473737714e594e5062687a4c32375a5954396b457271635a')
+      expect(version.arbitrator).toBe('0x28d0807903c5591c570173502d8507a65be64edb')
+      expect(version.appFeesCashier).toBe('0xa52fd5bf794c1e8b44ee7db7f277ad31c1c8afb4')
+      expect(version.effectiveFrom).toBe('1596140953')
     })
   })
 })
