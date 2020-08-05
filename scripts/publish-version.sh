ec4b17522de85c60c7d9e37622853a7f3e226f41
Pierre Bertet <hello@pierre.world>
2020-08-05 18:06:51 +0100

    Add a script to automate most of the publication tasks (#188)

---
+++ scripts/publish-version.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env sh
+set -eu
+
+# IMPORTANT: run this script from the root directory, it won’t work otherwise.
+
+confirm() {
+  while true; do
+    printf "%s [Y/n] " "$1"
+    read -r yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) echo "Exiting."; exit 0;;
+        * ) break;;
+    esac
+  done
+}
+
+package_readme() {
+  cat <<EOF
+# @$1 [<img height="100" align="right" alt="" src="https://user-images.githubusercontent.com/36158/85128259-d201f100-b228-11ea-9770-76ae86cc98b3.png">](https://connect.aragon.org/)
+
+[![](https://img.shields.io/github/package-json/v/$1?label=npm)](https://www.npmjs.com/package/@$1) [![](https://img.shields.io/bundlephobia/minzip/@$1)](https://bundlephobia.com/result?p=@$1) [![codecov](https://codecov.io/gh/aragon/connect/branch/master/graph/badge.svg)](https://codecov.io/gh/aragon/connect)
+
+Please have a look at the [documentation website](https://connect.aragon.org/) for Aragon Connect. If you never used the library before, we highly recommend starting with the [Getting Started](https://connect.aragon.org/guides/getting-started) guide.
+EOF
+}
+
+# Build everything
+echo "Building packages…"
+yarn build
+
+# Run the tests
+echo "Testing…"
+yarn test
+
+# Copy the README files in each package directory.
+confirm "Generate the README files?"
+for pkg_json in ./packages/*/package.json; do
+  pkg_name=$(basename "$(dirname "$pkg_json")")
+  package_readme "aragon/$pkg_name" > "packages/$pkg_name/README.md"
+done
+
+# Publish
+confirm "Publish a version of Connect?"
+./node_modules/.bin/oao publish
+
+# Push the version to GitHub
+tag="$(git describe --tags "$(git rev-list --tags --max-count=1)")"
+
+confirm "Create a version branch for $tag and push to GitHub?"
+
+echo "Push the master branch…"
+git push origin master
+
+echo "Create the $tag branch…"
+git checkout -b "$tag" "$tag"
+
+echo "Push the $tag tag…"
+git push origin "refs/tags/$tag"
+
+echo "Push the $tag branch…"
+git push origin "refs/heads/$tag:refs/heads/$tag"
+
+echo "Done."
