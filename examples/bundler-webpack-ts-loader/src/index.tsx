0f5c05a88ca85140bc4a5fc0d42c2b331ae33877
Pierre Bertet <hello@pierre.world>
2020-08-14 17:06:29 +0100

    Update bundler-webpack-ts-loader app

--- examples/bundler-webpack-ts-loader/src/index.tsx
+++ examples/bundler-webpack-ts-loader/src/index.tsx
@@ -1,21 +1,5 @@
-// import ReactDOM from 'react-dom'
-// import React from 'react'
-// import { Connect, useOrganization } from '@aragon/connect-react'
+import connect from '@aragon/connect'
 
-import * as ethers from 'ethers'
-console.log(
-  'ethers.getDefaultProvider():',
-  ethers.getDefaultProvider({ chainId: 1, name: 'homestead' })
-)
-
-// function App() {
-//   const [org] = useOrganization()
-//   return <div>Org address: {org ? org.address : 'loading…'}</div>
-// }
-
-// ReactDOM.render(
-//   <Connect location="pierre.aragonid.eth" connector="thegraph">
-//     <App />
-//   </Connect>,
-//   document.querySelector('#app')
-// )
+connect('a1.aragonid.eth', 'thegraph').then((org) => {
+  console.log('Organization:', org)
+})
