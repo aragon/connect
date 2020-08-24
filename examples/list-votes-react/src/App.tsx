436c437b21c7656b13a0f7dafb1002bd91c09a2c
Pierre Bertet <hello@pierre.world>
2020-08-24 11:01:17 +0100

    Update list-votes-react to display errors properly

--- examples/list-votes-react/src/App.tsx
+++ examples/list-votes-react/src/App.tsx
@@ -1,13 +1,18 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 import {
-  App as ConnectApp,
   Connect,
   createAppHook,
   useApp,
-  useApps,
   useOrganization,
 } from '@aragon/connect-react'
 import connectVoting, { Voting, Vote } from '@aragon/connect-voting'
+
+const NETWORK = 1
+const ORG_ADDRESS = 'governance.aragonproject.eth'
+const VOTING_APP_FILTER = 'voting'
+// const NETWORK = 4
+// const ORG_ADDRESS = '0x7cee20f778a53403d4fc8596e88deb694bc91c98'
+// const VOTING_APP_FILTER = '0xf7f9a33ed13b01324884bd87137609251b5f7c88'
 
 const VOTES_PER_PAGE = 5
 
@@ -15,7 +20,11 @@ const useVoting = createAppHook(connectVoting)
 
 export default function App() {
   return (
-    <Connect location="governance.aragonproject.eth" connector="thegraph">
+    <Connect
+      location={ORG_ADDRESS}
+      connector="thegraph"
+      options={{ network: NETWORK }}
+    >
       <Organization />
       <VotingApp />
       <Votes />
@@ -33,18 +42,28 @@ function Organization() {
 }
 
 function VotingApp() {
-  const [voting] = useApp('voting')
+  const [voting, votingStatus] = useApp(VOTING_APP_FILTER)
   return (
     <section>
-      <h1>Voting App</h1> <p>{voting?.address || 'loading…'}</p>
+      <h1>Voting App</h1>
+      <p>
+        {(() => {
+          if (votingStatus.loading) {
+            return 'loading…'
+          }
+          if (votingStatus.error) {
+            return votingStatus.error.message
+          }
+          return voting?.address
+        })()}
+      </p>
     </section>
   )
 }
 
 function Votes() {
-  const [voting, votingStatus] = useApp('voting')
+  const [voting, votingStatus] = useApp(VOTING_APP_FILTER)
   const [page, setPage] = useState<number>(0)
-
   const [votes, votesStatus] = useVoting(
     voting,
     (app: Voting) => {
@@ -64,13 +83,16 @@ function Votes() {
         }}
       >
         {(() => {
-          if (votingStatus.loading || votesStatus.loading) {
-            return <p>Loading votes…</p>
+          if (votingStatus.error) {
+            return <p>{votingStatus.error.message}</p>
           }
           if (votesStatus.error) {
-            return <p>Error: {votesStatus.error.message}</p>
+            return <p>{votesStatus.error.message}</p>
           }
-          if (votes) {
+          if (votingStatus.loading || votesStatus.loading) {
+            return <p>Loading…</p>
+          }
+          if (votes && votes.length > 0) {
             return (
               <ul>
                 {votes.map((vote: Vote) => (
@@ -79,7 +101,7 @@ function Votes() {
               </ul>
             )
           }
-          return <p>No result.</p>
+          return <p>No votes.</p>
         })()}
       </div>
       <div
