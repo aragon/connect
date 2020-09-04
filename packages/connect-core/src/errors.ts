4e4f22b99bbd238d7ab540e8187846276f81615f
Pierre Bertet <hello@pierre.world>
2020-09-04 21:10:46 +0100

    Errors: add ErrorUnexpectedResult and the variant param

--- packages/connect-core/src/errors.ts
+++ packages/connect-core/src/errors.ts
@@ -9,87 +9,122 @@ function defineNonEnumerable(instance: object, name: string, value: any) {
 type ErrorOptions = {
   code?: string
   name?: string
+  variant?: string
 }
 
 export class ErrorException extends Error {
   constructor(
     message = 'An unexpected error happened.',
-    { code = 'AC0001', name = 'ErrorException' }: ErrorOptions = {}
+    {
+      code = 'AC0001',
+      name = 'ErrorException',
+      variant = '',
+    }: ErrorOptions = {}
   ) {
     super(formatMessage(code, message))
     defineNonEnumerable(this, 'name', name)
     defineNonEnumerable(this, 'code', code)
+    defineNonEnumerable(this, 'variant', variant)
   }
 }
 
 export class ErrorInvalid extends ErrorException {
   constructor(
     message = 'The resource doesn’t seem to be valid.',
-    { code = 'AC0002', name = 'ErrorInvalid' }: ErrorOptions = {}
+    { code = 'AC0002', name = 'ErrorInvalid', variant }: ErrorOptions = {}
   ) {
-    super(message, { code, name })
+    super(message, { code, name, variant })
   }
 }
 
 export class ErrorUnsupported extends ErrorException {
   constructor(
     message = 'The resource is not supported.',
-    { code = 'AC0003', name = 'ErrorUnsupported' }: ErrorOptions = {}
+    { code = 'AC0003', name = 'ErrorUnsupported', variant }: ErrorOptions = {}
   ) {
-    super(message, { code, name })
+    super(message, { code, name, variant })
   }
 }
 
 export class ErrorNotFound extends ErrorException {
   constructor(
     message = 'The resource couldn’t be found.',
-    { code = 'AC0004', name = 'ErrorNotFound' }: ErrorOptions = {}
+    { code = 'AC0004', name = 'ErrorNotFound', variant }: ErrorOptions = {}
   ) {
-    super(message, { code, name })
+    super(message, { code, name, variant })
   }
 }
 
 export class ErrorConnection extends ErrorException {
   constructor(
     message = 'An error happened while communicating with a remote server.',
-    { code = 'AC0005', name = 'ErrorNotFound' }: ErrorOptions = {}
+    { code = 'AC0005', name = 'ErrorNotFound', variant }: ErrorOptions = {}
   ) {
-    super(message, { code, name })
+    super(message, { code, name, variant })
+  }
+}
+
+export class ErrorUnexpectedResult extends ErrorException {
+  constructor(
+    message = 'The resource doesn’t correspond to the expected result.',
+    {
+      code = 'AC0006',
+      name = 'ErrorUnexpectedResult',
+      variant,
+    }: ErrorOptions = {}
+  ) {
+    super(message, { code, name, variant })
   }
 }
 
 export class ErrorInvalidEthereum extends ErrorInvalid {
   constructor(
     message = 'The Ethereum provider doesn’t seem to be valid.',
-    { code = 'AC0006', name = 'ErrorInvalidEthereum' }: ErrorOptions = {}
+    {
+      code = 'AC0007',
+      name = 'ErrorInvalidEthereum',
+      variant,
+    }: ErrorOptions = {}
   ) {
-    super(message, { code, name })
+    super(message, { code, name, variant })
   }
 }
 
 export class ErrorInvalidLocation extends ErrorInvalid {
   constructor(
     message = 'The Ethereum address or ENS domain doesn’t seem to be valid.',
-    { code = 'AC0007', name = 'ErrorInvalidLocation' }: ErrorOptions = {}
+    {
+      code = 'AC0008',
+      name = 'ErrorInvalidLocation',
+      variant,
+    }: ErrorOptions = {}
   ) {
-    super(message, { code, name })
+    super(message, { code, name, variant })
   }
 }
 
 export class ErrorInvalidNetwork extends ErrorInvalid {
   constructor(
     message = 'The network doesn’t seem to be valid.',
-    { code = 'AC0008', name = 'ErrorInvalidNetwork' }: ErrorOptions = {}
+    {
+      code = 'AC0009',
+      name = 'ErrorInvalidNetwork',
+      variant,
+    }: ErrorOptions = {}
   ) {
-    super(message, { code, name })
+    super(message, { code, name, variant })
   }
 }
 
 export class ErrorUnsufficientBalance extends ErrorException {
   constructor(
-    message = 'Unsifficient balance on the account.',
-    { code = 'AC0009', name = 'ErrorUnsufficientBalance' }: ErrorOptions = {}
+    message = 'Unsufficient balance on the account.',
+    {
+      code = 'AC0010',
+      name = 'ErrorUnsufficientBalance',
+      variant,
+    }: ErrorOptions = {}
   ) {
-    super(message, { code, name })
+    super(message, { code, name, variant })
   }
 }
