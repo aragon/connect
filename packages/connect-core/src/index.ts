8a7360142b8bf1218f8465783dd86279ad75a313
0xGabi <gabrielpk.18@gmail.com>
2020-09-08 21:37:44 -0300

    Core: use artifact roles data to postprocess description

--- packages/connect-core/src/index.ts
+++ packages/connect-core/src/index.ts
@@ -13,6 +13,7 @@ export {
   ConnectorJsonConfig,
 } from './connections/ConnectorJson'
 export { default as App } from './entities/App'
+export { default as ForwardingPath } from './entities/ForwardingPath'
 export { default as Organization } from './entities/Organization'
 export { default as Permission } from './entities/Permission'
 export { default as Repo } from './entities/Repo'
@@ -20,6 +21,7 @@ export { default as Role } from './entities/Role'
 export {
   ConnectionContext,
   AppData,
+  ForwardingPathData,
   PermissionData,
   RepoData,
   RoleData,
