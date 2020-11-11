5f3f7a50d6fae2478c011792b44451e73866dcc6
0xGabi <gabrielpk.18@gmail.com>
2020-11-11 17:14:47 -0300

    Voting: fix double voting + use string for script arguments

--- packages/connect-voting/subgraph/src/Voting.ts
+++ packages/connect-voting/subgraph/src/Voting.ts
@@ -3,85 +3,26 @@ import {
   StartVote as StartVoteEvent,
   CastVote as CastVoteEvent,
   ExecuteVote as ExecuteVoteEvent,
-  Voting as VotingContract
+  Voting as VotingContract,
 } from '../generated/templates/Voting/Voting'
 import {
   Vote as VoteEntity,
-  Cast as CastEntity
+  Cast as CastEntity,
+  Voter as VoterEntity,
 } from '../generated/schema'
 
+/* eslint-disable @typescript-eslint/no-use-before-define */
+
 export function handleStartVote(event: StartVoteEvent): void {
-  let vote = _getVoteEntity(event.address, event.params.voteId)
+  const voteEntityId = buildVoteEntityId(event.address, event.params.voteId)
+  const vote = new VoteEntity(voteEntityId)
+  const voting = VotingContract.bind(event.address)
+  const voteData = voting.getVote(event.params.voteId)
 
-  _populateVoteDataFromEvent(vote, event)
-  _populateVoteDataFromContract(vote, event.address, vote.voteNum)
-
-  vote.save()
-}
-
-export function handleCastVote(event: CastVoteEvent): void {
-  let vote = _getVoteEntity(event.address, event.params.voteId)
-
-  let numCasts = vote.casts.length
-
-  let castId = _getCastEntityId(vote, numCasts)
-  let cast = new CastEntity(castId)
-
-  _populateCastDataFromEvent(cast, event)
-  cast.voteNum = vote.voteNum
-  cast.voteId = vote.id
-
-  let casts = vote.casts
-  casts.push(castId)
-  vote.casts = casts
-
-  if (event.params.supports == true) {
-    vote.yea = vote.yea.plus(event.params.stake)
-  } else {
-    vote.nay = vote.nay.plus(event.params.stake)
-  }
-
-  vote.save()
-  cast.save()
-}
-
-export function handleExecuteVote(event: ExecuteVoteEvent): void {
-  let vote = _getVoteEntity(event.address, event.params.voteId)
-
-  vote.executed = true
-
-  vote.save()
-}
-
-function _getVoteEntity(appAddress: Address, voteNum: BigInt): VoteEntity {
-  let voteEntityId = _getVoteEntityId(appAddress, voteNum)
-
-  let vote = VoteEntity.load(voteEntityId)
-  if (!vote) {
-    vote = new VoteEntity(voteEntityId)
-
-    vote.voteNum = voteNum
-    vote.executed = false
-    vote.casts = []
-  }
-
-  return vote!
-}
-
-function _getCastEntityId(vote: VoteEntity, numCast: number): string {
-  return vote.id + '-castNum:' + numCast.toString()
-}
-
-function _getVoteEntityId(appAddress: Address, voteNum: BigInt): string {
-  return 'appAddress:' + appAddress.toHexString() + '-voteId:' + voteNum.toHexString()
-}
-
-function _populateVoteDataFromContract(vote: VoteEntity, appAddress: Address, voteNum: BigInt): void {
-  let voting = VotingContract.bind(appAddress)
-
-  let voteData = voting.getVote(voteNum)
-
-  vote.executed = voteData.value1
+  vote.appAddress = event.address
+  vote.creator = event.params.creator
+  vote.metadata = event.params.metadata
+  vote.voteNum = event.params.voteId
   vote.startDate = voteData.value2
   vote.snapshotBlock = voteData.value3
   vote.supportRequiredPct = voteData.value4
@@ -89,18 +30,96 @@ function _populateVoteDataFromContract(vote: VoteEntity, appAddress: Address, vo
   vote.yea = voteData.value6
   vote.nay = voteData.value7
   vote.votingPower = voteData.value8
-  vote.script = voteData.value9
+  vote.script = voteData.value9.toHex()
   vote.orgAddress = voting.kernel()
-  vote.appAddress = appAddress
+  vote.executedAt = BigInt.fromI32(0)
+  vote.executed = false
+
+  vote.save()
 }
 
-function _populateVoteDataFromEvent(vote: VoteEntity, event: StartVoteEvent): void {
-  vote.creator = event.params.creator
-  vote.metadata = event.params.metadata
+export function handleCastVote(event: CastVoteEvent): void {
+  updateVoteState(event.address, event.params.voteId)
+
+  const voter = loadOrCreateVoter(event.address, event.params.voter)
+  voter.save()
+
+  const castVote = loadOrCreateCastVote(
+    event.address,
+    event.params.voteId,
+    event.params.voter
+  )
+
+  castVote.voter = voter.id
+  castVote.stake = event.params.stake
+  castVote.supports = event.params.supports
+  castVote.createdAt = event.block.timestamp
+
+  castVote.save()
 }
 
-function _populateCastDataFromEvent(cast: CastEntity, event: CastVoteEvent): void {
-  cast.voter = event.params.voter
-  cast.supports = event.params.supports
-  cast.voterStake = event.params.stake
+export function handleExecuteVote(event: ExecuteVoteEvent): void {
+  updateVoteState(event.address, event.params.voteId)
+
+  const vote = VoteEntity.load(
+    buildVoteEntityId(event.address, event.params.voteId)
+  )!
+  vote.executed = true
+  vote.executedAt = event.block.timestamp
+  vote.save()
+}
+
+function buildVoteEntityId(appAddress: Address, voteNum: BigInt): string {
+  return (
+    'appAddress:' +
+    appAddress.toHexString() +
+    '-voteId:' +
+    voteNum.toHexString()
+  )
+}
+
+function buildVoterId(voting: Address, voter: Address): string {
+  return voting.toHexString() + '-voter-' + voter.toHexString()
+}
+
+function buildCastEntityId(voteId: BigInt, voter: Address): string {
+  return voteId.toHexString() + '-voter:' + voter.toHexString()
+}
+
+function loadOrCreateVoter(
+  votingAddress: Address,
+  voterAddress: Address
+): VoterEntity {
+  const voterId = buildVoterId(votingAddress, voterAddress)
+  let voter = VoterEntity.load(voterId)
+  if (voter === null) {
+    voter = new VoterEntity(voterId)
+    voter.address = voterAddress
+  }
+  return voter!
+}
+
+function loadOrCreateCastVote(
+  votingAddress: Address,
+  voteId: BigInt,
+  voterAddress: Address
+): CastEntity {
+  const castVoteId = buildCastEntityId(voteId, voterAddress)
+  let castVote = CastEntity.load(castVoteId)
+  if (castVote === null) {
+    castVote = new CastEntity(castVoteId)
+    castVote.vote = buildVoteEntityId(votingAddress, voteId)
+  }
+  return castVote!
+}
+
+export function updateVoteState(votingAddress: Address, voteId: BigInt): void {
+  const votingApp = VotingContract.bind(votingAddress)
+  const voteData = votingApp.getVote(voteId)
+
+  const vote = VoteEntity.load(buildVoteEntityId(votingAddress, voteId))!
+  vote.yea = voteData.value6
+  vote.nay = voteData.value7
+
+  vote.save()
 }
