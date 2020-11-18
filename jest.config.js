e15297b5db9fa667746ff33ac18c9b8aa40c2c62
Gabi <gabrielpk.18@gmail.com>
2020-11-17 22:09:06 -0300

    Chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,10 @@ module.exports = {
   projects: [
     '<rootDir>/packages/connect-core/jest.config.js',
     '<rootDir>/packages/connect/jest.config.js',
-    // '<rootDir>/packages/connect-thegraph/jest.config.js',
+    '<rootDir>/packages/connect-thegraph/jest.config.js',
     '<rootDir>/packages/connect-react/jest.config.js',
     '<rootDir>/packages/connect-finance/jest.config.js',
-    // '<rootDir>/packages/connect-tokens/jest.config.js',
+    '<rootDir>/packages/connect-tokens/jest.config.js',
     '<rootDir>/packages/connect-voting/jest.config.js',
   ],
   coverageDirectory: '<rootDir>/coverage/',
