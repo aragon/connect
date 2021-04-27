7dff08dd6725eb0350bb26907accd6989a7ef9db
Gabriel <0xgabi@gmail.com>
2021-04-26 23:53:29 -0300

    Remove generated types folder

--- packages/connect-thegraph/types/parsers/repos.d.ts
+++
@@ -1,4 +0,0 @@
-import { RepoData } from '@1hive/connect-core'
-import { QueryResult } from '../types'
-export declare function parseRepo(result: QueryResult): RepoData
-//# sourceMappingURL=repos.d.ts.map
