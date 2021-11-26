507d205575ddfc8b78bb4545dd6dc70f3984ff97
0xGabi <gabrielpk.18@gmail.com>
2021-11-26 17:29:29 -0300

    Chore: update xdai default endpoint

--- packages/connect/src/constants.ts
+++ packages/connect/src/constants.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_IPFS_CACHED_ITEMS = 40
 export const DEFAULT_IPFS_URL = 'https://ipfs.io/ipfs/{cid}{path}'
 export const POLYGON_HTTP_ENDPOINT = 'https://polygon-rpc.com'
-export const XDAI_HTTP_ENDPOINT = 'https://xdai.1hive.org'
+export const XDAI_HTTP_ENDPOINT = 'https://rpc.xdaichain.com'
