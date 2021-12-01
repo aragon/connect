733f6a26245a2715a5a1d93a9a1e6cedf5421ebf
0xGabi <gabrielpk.18@gmail.com>
2021-12-01 19:42:56 -0300

    v0.9.8

--- examples/nodejs/src/describe-script.ts
+++ examples/nodejs/src/describe-script.ts
@@ -6,12 +6,81 @@ const NETWORK = 100
 async function main() {
   const org = await connect(ORG_ADDRESS, 'thegraph', { network: NETWORK })
 
-  const script =
-    '0x00000001a377585abed3e943e58174b55558a2482894ce2000000064beabacc80000000000000000000000003a97704a1b25f08aa230ae53b352e2e72ef528430000000000000000000000006626528de0c75ccc7a0d24f2d24b99060f74edee00000000000000000000000000000000000000000000001b1ae4d6e2ef500000'
+  const [apps, apps2, apps3, apps4, apps5, apps6] = await Promise.all([
+    org.apps(),
+    org.apps(),
+    org.apps(),
+    org.apps(),
+    org.apps(),
+    org.apps(),
+  ])
+  // const apps = await org.apps()
 
-  const description = await org.describeScript(script)
+  // const script =
+  //   '0x00000001a377585abed3e943e58174b55558a2482894ce2000000064beabacc80000000000000000000000003a97704a1b25f08aa230ae53b352e2e72ef528430000000000000000000000006626528de0c75ccc7a0d24f2d24b99060f74edee00000000000000000000000000000000000000000000001b1ae4d6e2ef500000'
 
-  console.log(description)
+  // const apps2 = await org.apps()
+  // const apps3 = await org.apps()
+  // const apps4 = await org.apps()
+  // const apps5 = await org.apps()
+  // const apps6 = await org.apps()
+
+  // const description = await org.describeScript(script)
+
+  console.log(
+    'First: ',
+    apps.map((app) => {
+      return {
+        name: app.name,
+        artifactName: app.artifact.appName,
+      }
+    })
+  )
+  console.log(
+    'Second: ',
+    apps2.map((app) => {
+      return {
+        name: app.name,
+        artifactName: app.artifact.appName,
+      }
+    })
+  )
+  console.log(
+    'Second: ',
+    apps3.map((app) => {
+      return {
+        name: app.name,
+        artifactName: app.artifact.appName,
+      }
+    })
+  )
+  console.log(
+    'Second: ',
+    apps4.map((app) => {
+      return {
+        name: app.name,
+        artifactName: app.artifact.appName,
+      }
+    })
+  )
+  console.log(
+    'Second: ',
+    apps5.map((app) => {
+      return {
+        name: app.name,
+        artifactName: app.artifact.appName,
+      }
+    })
+  )
+  console.log(
+    'Second: ',
+    apps6.map((app) => {
+      return {
+        name: app.name,
+        artifactName: app.artifact.appName,
+      }
+    })
+  )
 }
 
 main()
