6f5a8a67062b30d893c94503d72e2d7ad2ee14ee
barukimang <barukimang@gmail.com>
2022-10-21 15:33:59 +0300

    adding goerli at subscriptions-low-level.ts

--- examples/nodejs/src/subscriptions-low-level.ts
+++ examples/nodejs/src/subscriptions-low-level.ts
@@ -5,7 +5,7 @@ import { keepRunning } from './helpers'
 const DAO_ADDRESS = '0x059bcfbc477c46ab39d76c05b7b40f3a42e7de3b'
 const VOTING_APP_ADDRESS = '0xc73e86aab9d232495399d62fc80a36ae52952b81'
 const ALL_VOTING_SUBGRAPH_URL =
-  'https://api.thegraph.com/subgraphs/name/aragon/aragon-voting-rinkeby'
+  'https://api.thegraph.com/subgraphs/name/aragon/aragon-voting-goerli'
 
 async function main() {
   console.log('\nLow-level inspection of a voting app using subscriptions:')
@@ -32,7 +32,7 @@ async function main() {
       }
       console.log(JSON.stringify(results.data, null, 2))
       console.log(
-        `\nTry creating a new vote at https://rinkeby.aragon.org/#/${DAO_ADDRESS}/${VOTING_APP_ADDRESS}/`
+        `\nTry creating a new vote at https://goerli.aragon.org/#/${DAO_ADDRESS}/${VOTING_APP_ADDRESS}/`
       )
     }
   )
